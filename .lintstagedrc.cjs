bf135dbdf01e60b29c903a4fb21b7e958f42eec5
arty gvozdenkov <gvozdenkov@gmail.com>
2023-10-20 11:59:13 +0300

    fix: prettier rules, eslint imports, lint-stage add .cjs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,14 +1,13 @@
 module.exports = {
   // this will check Typescript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
 
-  // This will lint and format TypeScript and                                             //JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint --fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+  // This will lint and format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js|cjs)": (filenames) => [
+    `yarn eslint --fix ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
   ],
 
   // this will Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
-}
+  "**/*.(md|json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+};
